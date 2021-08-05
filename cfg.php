30825ed7dfd8e070ffeec4bc375ce327f7118a26
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2021-08-05 14:54:24 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -5,10 +5,10 @@ pkg install imagemagick
 pkg install tesseract
 */
 
-$UA = "xxxxxxx";
+$UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.62";
 
-$COOKIE = "xxxxxxxx";
+$COOKIE = "PHPSESSID=4e3af1a9f23cb97610a0e9cc99f69fa2; _ga=GA1.2.2020003382.1628148476; _gid=GA1.2.664324981.1628148476; _gat_gtag_UA_151661461_1=1; SesHashKey=amiy3lm87h8o30pl; SesToken=ses_id%3D4429%26ses_key%3Damiy3lm87h8o30pl; AccExist=4429";
 
-$SOLVEMEDIA = "xxxxxxx";
+$SOLVEMEDIA = "https://api-secure.solvemedia.com/papi/_challenge.js?k=WHx3UGDFc-pSG5USBRCcorQmj9JijaLj;f=_ACPuzzleUtil.callbacks%5B0%5D;l=en;t=img;s=standard;c=js,h5c,h5ct,svg,h5v,v/h264,v/ogg,v/webm,h5a,a/mp3,a/ogg,ua/chrome,ua/chrome92,os/nt,os/nt10.0,expand,fwv/BX70fw.dbwe96,jslib/jquery,adblk,htmlplus;am=lBEiHAkAIH-Rb9SFCQAgfw;ca=ajax;ts=1628147738;ct=1628148476;th=white;r=0.8199000424921097";
 
 ?>
